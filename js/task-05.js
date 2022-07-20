3dac7e9fbf7682473dbd7ebf2adca34a2670df74
Ihor Podvalniy <bembasua@gmail.com>
2022-07-20 10:55:25 +0300

    багфікс

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,9 @@
 const inputEl = document.querySelector(`#name-input`);
 const nameEl = document.querySelector(`#name-output`);
 inputEl.addEventListener(`input`, (event) => {
-  nameEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value) {
+    nameEl.textContent = event.currentTarget.value;
+  } else {
+    nameEl.textContent = "Anonymous";
+  }
 });
